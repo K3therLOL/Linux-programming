7432957e5ca0842719c93c2d71ad5ddd8e1f7ce7
Kether <g3roylol@gmail.com>
2025-03-16 17:33:13 +0000

    fixed dirs

--- 1-library/compile-linking/classwork/hello.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "hello.h"
-
-void hello_msg(const char *name)
-{
-    printf("Hello, %s!\n", name);
-}
--- 1-library/compile-linking/classwork/hello.h
+++
@@ -1,6 +0,0 @@
-#ifndef __HELLO__
-#define __HELLO__
-
-void hello_msg(const char *);
-
-#endif
