64af17bfeb48bcfdb2029ea1392f8f3c1aebd723
Kether <g3roylol@gmail.com>
2025-03-20 20:37:01 +0000

    static library examples

--- 1-chapter-library/2-compile-linking/homework/solution.c
+++
@@ -1,8 +0,0 @@
-#include <stddef.h>
-#include <string.h>
-
-int stringStat(const char *string, size_t multiplier, int *count)
-{
-    (*count)++;
-    return strlen(string) * multiplier;
-}
---
+++ 1-chapter-library/static/obj/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void hello_msg(const char *name)
+{
+    printf("Hello, %s!\n", name);
+}
