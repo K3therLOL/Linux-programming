82f3ae9c0de73fe565b942d42758f5f1ee7c5826
K3therLOL <g3roylol@gmail.com>
2025-07-01 16:29:01 +0300

    multiplexing completed

--- 3-chapter-processes/ipc/multiplexing/solution.c
+++ 3-chapter-processes/ipc/multiplexing/solution.c
@@ -1,5 +1,6 @@
 /* Задание:
 * В текущем каталоге есть 2 канала in1 in2, в которые в случайном порядке поступают числа, которые необходимо просуммировать и вывести окончательную сумму на экран. Сумма выводится в отдельной строке, завершающейся символом конца строки. Признаком окончания подачи символов в каналы является закрытие этих каналов посылающей стороной. */
+
 #define _GNU_SOURCE
 #include <stdlib.h>
 #include <stdio.h>
@@ -9,7 +10,7 @@
 #include <unistd.h>
 #include <sys/select.h>
 
-int file_sum(int fd)
+int file_sum(int fd, fd_set *read_set)
 {
 
     int cap = fcntl(fd, F_GETPIPE_SZ);
@@ -26,21 +27,22 @@ int file_sum(int fd)
         perror("size");
         exit(EXIT_FAILURE);
     } else if(size == 0) {
-        fd = -1;
+        close(fd);
+        FD_CLR(fd, read_set);
         return 0;
     }
 
     content[size] = '\0';
     char *delim = " \t\n";
     int sum = 0;
-    for (char *token = strtok(content, delim); token != NULL; token = strtok(NULL, delim)) {
+    char *token;
+    for (token = strtok(content, delim); token != NULL; token = strtok(NULL, delim)) {
         int num;
         if ((num = atoi(token)) != 0) {
             sum += num;
         }
     }
     
-    printf("file_sum = %d\n", sum);
     free(content);
     return sum;
 }
@@ -48,42 +50,40 @@ int file_sum(int fd)
 void initialize_set(fd_set *set, int f1, int f2)
 {
     FD_ZERO(set);
-    if (f1 != -1) {
-        FD_SET(f1, set);
-    }
-
-    if (f2 != -1) {
-        FD_SET(f2, set);
-    }
+    FD_SET(f1, set);
+    FD_SET(f2, set);
 }
 
 int main()
 {
-    int f1 = open("./in1", O_RDWR);
-    int f2 = open("./in2", O_RDWR);
+    int f1 = open("./in1", O_RDONLY);
+    int f2 = open("./in2", O_RDONLY);
     if (f1 == -1 || f2 == -1) {
         perror("open");
         return EXIT_FAILURE;
     }
 
-    fd_set read_set;
+    fd_set read_set, inset;
+    initialize_set(&read_set, f1, f2);
     int sum = 0;
-    while ((f1 = open("./in1", O_RDWR)) != -1 || (f2 = open("./in2", O_RDWR)) != -1)
+    while (FD_ISSET(f1, &read_set) || FD_ISSET(f2, &read_set))
     {
+        // printf("%d %d\n", f1, f2);
         int max_fd = f1 > f2 ? f1 : f2;
-        initialize_set(&read_set, f1, f2);
-        if(select(max_fd + 1, &read_set, NULL, NULL, NULL) == -1) {
+        inset = read_set;
+        if (select(max_fd + 1, &inset, NULL, NULL, NULL) == -1) {
             perror("select");
             return EXIT_FAILURE;
         }
 
-        if (f1 != -1 && FD_ISSET(f1, &read_set)) {
-            sum += file_sum(f1);
+        if (FD_ISSET(f1, &inset)) {
+            sum += file_sum(f1, &read_set);
         }
 
-        if (f2 != -1 && FD_ISSET(f2, &read_set)) {
-            sum += file_sum(f2);
+        if (FD_ISSET(f2, &inset)) {
+            sum += file_sum(f2, &read_set);
         }
+
     }
 
     printf("%d\n", sum);
